4ad5f1d58f12ba234ba6c12a97e3207fe2e985b8
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-11-10 10:13:54 +0300

    Add files via upload
    https://studii.md/simpals/media/studii.md/0ccca41788a4590ed8a7398c5f64aaab.pdf

---
+++ Problema_1.py
@@ -0,0 +1,9 @@
+n=int(input('n='))
+if n==28 or n==29:
+    print('februarie') 
+if n==30:
+    print('aprilie','iunie','septembrie','noiembrie')   
+if n==31:
+    print('ianuarie','martie','mai','iulie','august','octombrie','decembrie') 
+if n<28 or n>31:
+    print('numar de zile invalid')
